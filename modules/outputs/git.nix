dcc549ed6e98792c109cec5b61bae07978b3ddd8
Jonathan Knapp <jon@coffeeandcode.com>
2026-01-16 15:31:24 -0500

    Example of adding `apps` to the flake

---
+++ modules/outputs/git.nix
@@ -0,0 +1,41 @@
+{
+  inputs,
+  ...
+}:
+let
+  wrappedGit =
+    pkgs:
+    inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.git;
+      runtimeInputs = [ pkgs._1password-gui ];
+      env = {
+        SSH_AUTH_SOCK = "$HOME/.1password/agent.sock";
+      };
+      preHook = ''
+        setsid 1password --silent </dev/null >/dev/null 2>&1 &
+        sleep 1
+      '';
+    };
+in
+{
+  perSystem =
+    { pkgs, system, ... }:
+    let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate =
+          pkg:
+          builtins.elem (pkgs.lib.getName pkg) [
+            "1password"
+          ];
+      };
+    in
+    {
+      apps.git = {
+        meta.description = "git + 1password ssh key support";
+        type = "app";
+        program = wrappedGit pkgs;
+      };
+    };
+}
