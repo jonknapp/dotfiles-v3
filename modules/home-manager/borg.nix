580856450576c8464912441b0731abe905e28101
Jonathan Knapp <jon@coffeeandcode.com>
2026-04-28 03:41:24 -0400

    Remove duplicated, incorrect location code

--- modules/home-manager/borg.nix
+++ modules/home-manager/borg.nix
@@ -18,23 +18,4 @@
         pika-backup
       ];
     };
-
-  flake.modules.homeManager.nixToolbox =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    {
-      home.packages = [
-        (lib.hiPrio (
-          pkgs.runCommand "chromium-browser-desktop-modify" { } ''
-            mkdir -p $out/share/applications
-            substitute ${pkgs.chromium}/share/applications/chromium-browser.desktop $out/share/applications/chromium-browser.desktop \
-              --replace-fail "Exec=chromium" "Exec=toolbox run --container ${config.programs.nixToolbox.containerName} chromium"
-          ''
-        ))
-      ];
-    };
 }
