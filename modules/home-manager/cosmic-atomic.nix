543da5cf068181d4f667034f36f7e5ea1d7b6b3e
Jonathan Knapp <jon@coffeeandcode.com>
2026-08-09 08:14:19 -0400

    Attempt to fix graphics issue on amd framework

--- modules/home-manager/cosmic-atomic.nix
+++ modules/home-manager/cosmic-atomic.nix
@@ -16,6 +16,11 @@
 
       home.homeDirectory = "/var/home/${config.home.username}";
 
+      home.sessionVariables = {
+        # https://github.com/pop-os/cosmic-comp/issues/2336
+        COSMIC_DISABLE_DIRECT_SCANOUT = "y";
+      };
+
       programs.nixToolbox.enable = true;
     };
 }
