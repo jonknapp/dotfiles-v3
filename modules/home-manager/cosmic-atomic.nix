94fad21aa6faf1c1bfb723553c441a295ed42dcd
Jonathan Knapp <jon@coffeeandcode.com>
2026-08-11 07:27:21 -0400

    Revert "Attempt to fix graphics issue on amd framework"
    This reverts commit 543da5cf068181d4f667034f36f7e5ea1d7b6b3e.

--- modules/home-manager/cosmic-atomic.nix
+++ modules/home-manager/cosmic-atomic.nix
@@ -16,11 +16,6 @@
 
       home.homeDirectory = "/var/home/${config.home.username}";
 
-      home.sessionVariables = {
-        # https://github.com/pop-os/cosmic-comp/issues/2336
-        COSMIC_DISABLE_DIRECT_SCANOUT = "y";
-      };
-
       programs.nixToolbox.enable = true;
     };
 }
